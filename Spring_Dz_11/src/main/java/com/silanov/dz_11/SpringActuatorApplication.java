813eecd19c854de302b02ffb3e6cd0e52c18dab3
DmitrySilanov <silapov@yandex.ru>
2025-01-28 02:14:40 +0300

    Add files Spring_Dz_11

---
+++ Spring_Dz_11/src/main/java/com/silanov/dz_11/SpringActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.silanov.dz_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorApplication.class, args);
+	}
+
+}
---
+++ Spring_Dz_11/src/test/java/com/silanov/dz_11/MySpringActuatorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silanov.dz_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringActuatorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
